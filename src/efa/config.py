886aae1d417d2f51a890cfde7d3539b6630e7895
Het Parekh <hetparekh16@gmail.com>
2025-11-18 21:44:34 +0100

    updated variable name

--- src/efa/config.py
+++ src/efa/config.py
@@ -4,5 +4,4 @@ forecast_hours = list(range(0, 49))  # 0 to 48 hours
 lat = 53.495
 lon = 10.011
 
-data_path = "data/historical_data"
-output_path = "data/processed"
+dwd_data_path = "data/historical_data"
