62b733b3e5e57c7e422c05b1aefaa7689ece0da5
Het Parekh <hetparekh16@gmail.com>
2025-11-24 16:19:50 +0100

    added more locations to config

--- src/efa/config.py
+++ src/efa/config.py
@@ -1,8 +1,13 @@
-
 forecast_hours = list(range(0, 49))  # 0 to 48 hours 
 
-lat = 53.495
-lon = 10.011
+# Multiple locations (lat, lon pairs)
+locations = [
+    {'lat': 53.908585, 'lon': 9.193248},   # Location 1
+    {'lat': 53.518114, 'lon': 9.918907},   # Location 2
+    {'lat': 49.735281, 'lon': 9.703521},   # Neubrunn
+    {'lat': 51.160670, 'lon': 12.410260},  # Energiepark
+]
+
 
 dwd_data_path = "data/historical_data"
 remote_path = 'smb://triton.ieet.tuhh.de/dwd-data/'
